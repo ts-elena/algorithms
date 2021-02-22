93f683943ad6787bec1be9e5c6dad58794533b83
TSELLER Elena <Elena.S.TSELLER@raiffeisen.ru>
2021-02-22 21:03:50 +0300

    temporary remove homework files

--- src/22.02.2020/fibonacci.ts
+++
@@ -1,9 +0,0 @@
-export const fib = (n: number): number => {
-  let next: number = 0;
-  let prev: number = 1;
-  for (let i: number = 0; i <= n; i++) {
-    next = prev + next;
-    prev = next - prev;
-  }
-  return prev;
-};
--- src/22.02.2020/findAllWholeNumberDividers.ts
+++
@@ -1,9 +0,0 @@
-export const findAllWholeNumberDividers = (num: number): number[] => {
-  const dividers: number[] = [];
-  for (let i: number = 1; i <= num; i++) {
-    if (num % i === 0) {
-      dividers.push(i);
-    }
-  }
-  return dividers;
-};
